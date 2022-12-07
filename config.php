573c977d1e494225b623acb04738f74a0cb59622
Jordan Thompson <twjordan29@gmail.com>
2022-12-06 22:04:33 -0330

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php
-
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 session_start();
 
 $conn = mysqli_connect("localhost", "root", "R61HGVkAuYGGsr", "cardropdown");
